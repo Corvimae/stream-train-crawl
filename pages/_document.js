36fad4430e32d95f19b1bc7eb58a2c1191b0f459
May Roussel <may@maybreak.com>
2021-06-30 11:04:29 -0500

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -33,6 +33,7 @@ class DJFlygonDocument extends Document {
         <Head>
           <title>dj flygon servin' up fresh dual wingbeats</title>
           <link href="https://fonts.googleapis.com/css?family=Assistant:400,700&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" />
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
